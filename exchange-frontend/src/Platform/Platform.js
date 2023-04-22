7d474a2f40a30b26565c452d5a574d53662163f5
MohamadKhalil430L <mhk54@mail.aub.edu>
2023-04-22 22:06:22 +0300

    Added Friends List

--- exchange-frontend/src/Platform/Platform.js
+++ exchange-frontend/src/Platform/Platform.js
@@ -17,7 +17,9 @@ import {FaUserCircle} from 'react-icons/fa'
 const Platform = () => {
     let userToken = getUserToken();
     let users = ['Mohamad', 'Ralph', 'Tamer'];
-    const [filteredList, setFilteredList] = new useState(users);
+    let friends = ['Khalil', 'Abou Nader', 'Boutros'];
+    const [filteredListUsers, setfilteredListUsers] = new useState(users);
+    const [filteredListFriends, setfilteredListFriends] = new useState(friends);
     //from material ui
     const [tabValue, setTabValue] = useState("");
 
@@ -25,13 +27,20 @@ const Platform = () => {
         setTabValue(newValue);
     };
 
-    const [searchTerm, setSearchTerm] = useState("");
+    const [searchTermUsers, setsearchTermUsers] = useState("");
+    const [searchTermFriends, setsearchTermFriends] = useState("");
 
-    const handleSearch = () => {
+    const handleSearchUsers = () => {
         let updatedList = [...users];
-        updatedList = updatedList.filter((item) => item.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1);
-        setFilteredList(updatedList);
-        console.log(searchTerm)
+        updatedList = updatedList.filter((item) => item.toLowerCase().indexOf(searchTermUsers.toLowerCase()) !== -1);
+        setfilteredListUsers(updatedList);
+    };
+
+    const handleSearchFriends = () => {
+        let updatedListFriends = [...friends];
+        updatedListFriends = updatedListFriends.filter((item) => item.toLowerCase().indexOf(searchTermFriends.toLowerCase()) !== -1);
+        setfilteredListFriends(updatedListFriends);
+        console.log(updatedListFriends)
     };
 
     return (
@@ -64,24 +73,18 @@ const Platform = () => {
                     </TabPanel>
 
                     <TabPanel value='2'>
-                        <div className="platform__cards">
-                            no friends
-                        </div>
-                    </TabPanel>
-
-                    <TabPanel value='3'>
-                        <div className="platform__cards">
+                    <div className="platform__cards">
                             <TextField
                                 fullWidth
                                 placeholder="Search..."
-                                value={searchTerm}
+                                value={searchTermFriends}
                                 onChange={({ target: { value } }) => {
-                                    setSearchTerm(value)
+                                    setsearchTermFriends(value)
                                     if (!value) {
-                                        setFilteredList(users)
+                                        setfilteredListFriends(friends)
                                     }
                                     else{
-                                        handleSearch()
+                                        handleSearchFriends()
                                     }
                                 }}
                                 InputProps={{
@@ -95,7 +98,50 @@ const Platform = () => {
                             />
                             <div>
                                 <ol>
-                                    {filteredList.map((item) => (
+                                    {filteredListFriends.map((item) => (
+                                        <li className='users__cards'>
+                                            <FaUserCircle className='icon'/>
+                                            <span>
+                                                {item}
+                                            </span>
+                                            <div className='friends__buttons'>
+                                            <Button className='button'  variant="contained" size='small' style={{ backgroundColor: "white", color: "#2c2c6c", fontWeight: "bold", width: "fit-content", marginInline: "4px"}}>Record</Button>
+                                            <Button className='button'  variant="contained" size='small' style={{ backgroundColor: "red", color: 'white', fontWeight: "bold", width: "fit-content"}}>Remove</Button>
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ol>
+                            </div>
+                        </div>
+                    </TabPanel>
+
+                    <TabPanel value='3'>
+                        <div className="platform__cards">
+                            <TextField
+                                fullWidth
+                                placeholder="Search..."
+                                value={searchTermUsers}
+                                onChange={({ target: { value } }) => {
+                                    setsearchTermUsers(value)
+                                    if (!value) {
+                                        setfilteredListUsers(users)
+                                    }
+                                    else{
+                                        handleSearchUsers()
+                                    }
+                                }}
+                                InputProps={{
+                                    startAdornment: <BiSearch className='icon'/>,
+                                    style: { color: 'white' },
+                                }}
+                                InputLabelProps={{
+                                    style: { color: 'white' },
+                                }}
+
+                            />
+                            <div>
+                                <ol>
+                                    {filteredListUsers.map((item) => (
                                         <li className='users__cards'>
                                             <FaUserCircle className='icon'/>
                                             <span>
